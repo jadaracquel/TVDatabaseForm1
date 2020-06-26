661db3f9019ff71b6733edf8eb56f19914039134
Jada Racquel <mccoyjada.9816@gmail.com>
2020-06-25 20:18:17 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace TVDatabaseForm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.title_txtbx = new System.Windows.Forms.TextBox();
+            this.yr_txtbx = new System.Windows.Forms.TextBox();
+            this.genre_txtbx = new System.Windows.Forms.TextBox();
+            this.time_txtbx = new System.Windows.Forms.TextBox();
+            this.rating_txtbx = new System.Windows.Forms.TextBox();
+            this.id_label = new System.Windows.Forms.Label();
+            this.id_txtbx = new System.Windows.Forms.TextBox();
+            this.addEntry_btn = new System.Windows.Forms.Button();
+            this.showEntries_btn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.clear_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(659, 351);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 426);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 455);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Genre(s)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 486);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Duration(mins)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(60, 512);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Rated Audience";
+            // 
+            // title_txtbx
+            // 
+            this.title_txtbx.Location = new System.Drawing.Point(102, 400);
+            this.title_txtbx.Name = "title_txtbx";
+            this.title_txtbx.Size = new System.Drawing.Size(306, 20);
+            this.title_txtbx.TabIndex = 7;
+            // 
+            // yr_txtbx
+            // 
+            this.yr_txtbx.Location = new System.Drawing.Point(102, 426);
+            this.yr_txtbx.Name = "yr_txtbx";
+            this.yr_txtbx.Size = new System.Drawing.Size(105, 20);
+            this.yr_txtbx.TabIndex = 8;
+            // 
+            // genre_txtbx
+            // 
+            this.genre_txtbx.Location = new System.Drawing.Point(131, 454);
+            this.genre_txtbx.Name = "genre_txtbx";
+            this.genre_txtbx.Size = new System.Drawing.Size(186, 20);
+            this.genre_txtbx.TabIndex = 9;
+            // 
+            // time_txtbx
+            // 
+            this.time_txtbx.Location = new System.Drawing.Point(167, 486);
+            this.time_txtbx.Name = "time_txtbx";
+            this.time_txtbx.Size = new System.Drawing.Size(101, 20);
+            this.time_txtbx.TabIndex = 10;
+            // 
+            // rating_txtbx
+            // 
+            this.rating_txtbx.Location = new System.Drawing.Point(175, 512);
+            this.rating_txtbx.Name = "rating_txtbx";
+            this.rating_txtbx.Size = new System.Drawing.Size(81, 20);
+            this.rating_txtbx.TabIndex = 11;
+            // 
+            // id_label
+            // 
+            this.id_label.AutoSize = true;
+            this.id_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.id_label.Location = new System.Drawing.Point(60, 370);
+            this.id_label.Name = "id_label";
+            this.id_label.Size = new System.Drawing.Size(21, 17);
+            this.id_label.TabIndex = 12;
+            this.id_label.Text = "ID";
+            // 
+            // id_txtbx
+            // 
+            this.id_txtbx.Location = new System.Drawing.Point(104, 369);
+            this.id_txtbx.Name = "id_txtbx";
+            this.id_txtbx.Size = new System.Drawing.Size(105, 20);
+            this.id_txtbx.TabIndex = 13;
+            // 
+            // addEntry_btn
+            // 
+            this.addEntry_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addEntry_btn.Location = new System.Drawing.Point(175, 540);
+            this.addEntry_btn.Name = "addEntry_btn";
+            this.addEntry_btn.Size = new System.Drawing.Size(134, 35);
+            this.addEntry_btn.TabIndex = 14;
+            this.addEntry_btn.Text = "Add New Entry";
+            this.addEntry_btn.UseVisualStyleBackColor = true;
+            this.addEntry_btn.Click += new System.EventHandler(this.addEntry_btn_Click);
+            // 
+            // showEntries_btn
+            // 
+            this.showEntries_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showEntries_btn.Location = new System.Drawing.Point(477, 369);
+            this.showEntries_btn.Name = "showEntries_btn";
+            this.showEntries_btn.Size = new System.Drawing.Size(134, 35);
+            this.showEntries_btn.TabIndex = 15;
+            this.showEntries_btn.Text = "Show Entries";
+            this.showEntries_btn.UseVisualStyleBackColor = true;
+            this.showEntries_btn.Click += new System.EventHandler(this.showEntries_btn_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(215, 372);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "(ID Format: 123abc)";
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear_btn.Location = new System.Drawing.Point(63, 551);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(98, 24);
+            this.clear_btn.TabIndex = 17;
+            this.clear_btn.Text = "CLEAR FIELDS";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(685, 584);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.showEntries_btn);
+            this.Controls.Add(this.addEntry_btn);
+            this.Controls.Add(this.id_txtbx);
+            this.Controls.Add(this.id_label);
+            this.Controls.Add(this.rating_txtbx);
+            this.Controls.Add(this.time_txtbx);
+            this.Controls.Add(this.genre_txtbx);
+            this.Controls.Add(this.yr_txtbx);
+            this.Controls.Add(this.title_txtbx);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Movie Database Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox title_txtbx;
+        private System.Windows.Forms.TextBox yr_txtbx;
+        private System.Windows.Forms.TextBox genre_txtbx;
+        private System.Windows.Forms.TextBox time_txtbx;
+        private System.Windows.Forms.TextBox rating_txtbx;
+        private System.Windows.Forms.Label id_label;
+        private System.Windows.Forms.TextBox id_txtbx;
+        private System.Windows.Forms.Button addEntry_btn;
+        private System.Windows.Forms.Button showEntries_btn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button clear_btn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace TVDatabaseForm1
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt1 = new DataTable();
+        DataTable dt2 = new DataTable();
+        string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source='G:\SUMMER2020\TVDatabaseForm1\TVDatabaseA2.accdb'";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addEntry_btn_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                conn.Open();
+                string id = id_txtbx.Text;
+                string title = title_txtbx.Text;
+                int year = int.Parse(yr_txtbx.Text);
+                string genre = genre_txtbx.Text;
+                int minutes = int.Parse(time_txtbx.Text);
+                string rate = rating_txtbx.Text;
+/*
+                    OleDbCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = InsertMovie(id, title, year, genre, minutes, rate);
+                    cmd.ExecuteNonQuery();
+                }
+
+                string InsertMovie(string id, string title, int year, string genre, int minutes, string rate)
+                {
+                    return string.Format("INSERT INTO Movies VALUES ('{0}', '{1}', {2}, '{3}', {4}, '{5}');",
+                                        id, title, year, genre, minutes, rate);
+                }
+            }
+
+*/
+            OleDbCommand cmd = conn.CreateCommand();
+                cmd.CommandText = string.Format("INSERT INTO Movies VALUES ('{0}', '{1}', {2}, '{3}', {4}, '{5}');", 
+                    id, title, year, genre, minutes, rate);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void showEntries_btn_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                conn.Open();
+                OleDbCommand cmdA = conn.CreateCommand();
+                cmdA.CommandText = "SELECT * FROM Movies;";
+                using (OleDbDataReader myDReader = cmdA.ExecuteReader())
+                {
+                    dt1.Load(myDReader);
+                    dataGridView1.DataSource = dt1;
+                }
+            }
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            id_txtbx.Text = "";
+            title_txtbx.Text = "";
+            yr_txtbx.Text = "";
+            genre_txtbx.Text = "";
+            time_txtbx.Text = "";
+            rating_txtbx.Text = "";
+        }
+    }
+}
